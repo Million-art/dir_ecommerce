cb87f10624512476d8dc568da9d1916d751d2801
million-art <millionmulugeta09@gmail.com>
2024-06-26 00:47:41 +0300

    type fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import Navbar from '@/components/Navbar/page';
 import Home from './home/page';
 import Providers from './StoreProvider';
 
-const Index: React.FC = ({ children }:any) => {
+const Index: React.FC = ({children}:any) => {
+  const [showCart, setShowCart] = useState(false);
+
   return (
     <div className="h-full w-full">
-      <Navbar />
+      <Navbar setShowCart={setShowCart} />
       <Home />
       <Providers>{children}</Providers>
     </div>
